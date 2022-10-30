68da3cdfbe2c8483379f6c62508fea495fe5ca2f
Aliwoto <woto@kaizoku.cyou>
2022-10-30 22:28:47 +0330

    Add comment headers.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- wotoCrypto/passContainer/helpers.go
+++ wotoCrypto/passContainer/helpers.go
@@ -1,1 +1,6 @@
+// wotoCrypto Project
+// Copyright (C) 2022 ALiwoto
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of the source code.
+
 package passContainer
--- wotoCrypto/passContainer/methods.go
+++ wotoCrypto/passContainer/methods.go
@@ -1,3 +1,8 @@
+// wotoCrypto Project
+// Copyright (C) 2022 ALiwoto
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of the source code.
+
 package passContainer
 
 import "hash"
